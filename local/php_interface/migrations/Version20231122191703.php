7277efa7a873dddab780e7e7ee1908752a07ff6f
Irina Stockaya <i.stockaya@mcart.ru>
2023-11-23 18:41:18 +0300

    task151967 +migration

---
+++ local/php_interface/migrations/Version20231122191703.php
@@ -0,0 +1,362 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20231122191703 extends Version
+{
+    protected $description = "Миграция для блока Агенты по недвижимости";
+
+    protected $moduleVersion = "4.6.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+    $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'Agents',
+  'TABLE_NAME' => 'b_estate_agents',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Агенты по недвижимости',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'Real estate agents',
+    ),
+  ),
+));
+    $helper->Hlblock()->saveGroupPermissions($hlblockId, array (
+  'administrators' => 'W',
+  7 => 'R',
+  6 => 'R',
+  'content_editor' => 'R',
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_NAME',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'Y',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Agents name',
+    'ru' => 'ФИО агента',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_ACTIVITY',
+  'USER_TYPE_ID' => 'boolean',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'DEFAULT_VALUE' => 1,
+    'DISPLAY' => 'DROPDOWN',
+    'LABEL' => 
+    array (
+      0 => '',
+      1 => '',
+    ),
+    'LABEL_CHECKBOX' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Activity',
+    'ru' => 'Активность',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_EMAIL',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Email',
+    'ru' => 'Электронная почта',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_PHONE',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Phone',
+    'ru' => 'Телефон',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_ACTIVITY_TYPE',
+  'USER_TYPE_ID' => 'enumeration',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'DISPLAY' => 'LIST',
+    'LIST_HEIGHT' => 4,
+    'CAPTION_NO_VALUE' => '',
+    'SHOW_NO_VALUE' => 'N',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Type of activity',
+    'ru' => 'Вид деятельности',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ENUM_VALUES' => 
+  array (
+    0 => 
+    array (
+      'VALUE' => 'Брокер',
+      'DEF' => 'N',
+      'SORT' => '500',
+      'XML_ID' => 'broker',
+    ),
+    1 => 
+    array (
+      'VALUE' => 'Агент по продаже',
+      'DEF' => 'N',
+      'SORT' => '500',
+      'XML_ID' => 'saleagent',
+    ),
+    2 => 
+    array (
+      'VALUE' => 'Агент по покупке',
+      'DEF' => 'N',
+      'SORT' => '500',
+      'XML_ID' => 'buyagent',
+    ),
+    3 => 
+    array (
+      'VALUE' => 'Риэлтор',
+      'DEF' => 'N',
+      'SORT' => '500',
+      'XML_ID' => 'realtor',
+    ),
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_AGENT_PHOTO',
+  'USER_TYPE_ID' => 'file',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'LIST_WIDTH' => 0,
+    'LIST_HEIGHT' => 0,
+    'MAX_SHOW_SIZE' => 0,
+    'MAX_ALLOWED_SIZE' => 0,
+    'EXTENSIONS' => 
+    array (
+      'jpg' => true,
+      'png' => true,
+      'jpeg' => true,
+    ),
+    'TARGET_BLANK' => 'Y',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Photo',
+    'ru' => 'Фотография',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        }
+
+    public function down()
+    {
+      $helper = $this->getHelperManager();
+      $hlblockId = $helper->Hlblock()->getHlblockIdIfExists('Agents');
+
+      $helper->Hlblock()->deleteHlblock($hlblockId);
+    }
+}
