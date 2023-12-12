c09f079a37f9729216e7fd6ad0854f0c8d5f608c
Irina Stockaya <i.stockaya@mcart.ru>
2023-12-12 13:07:14 +0300

    task 151970 доработка

--- local/php_interface/include/add_user.php
+++ local/php_interface/include/add_user.php
@@ -1,9 +1,14 @@
-<?
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
 class UserToGroup {    
+
     public static function addUser(&$arFields) {
+
+        $context = \Bitrix\Main\Application::getInstance()->getContext();
+        $request = $context->getRequest();
+        $values = $request->getPostList();
         $user_id = $arFields["ID"];
-        $user_role = $arFields["UF_USER_ROLE"][0];
-        echo ($user_role);
+        $user_role = $values["UF_USER_ROLE"];
         if ($user_role == '7') {
             $group_id = array(6);
         }
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,4 +1,10 @@
-<?
-require_once("include/add_user.php");
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+\Bitrix\Main\Loader::registerAutoLoadClasses(
+    null,
+    [
+        'UserToGroup' => '/local/php_interface/include/add_user.php',
+    ]   
+);
 
 AddEventHandler("main", "OnAfterUserAdd", Array("UserToGroup","addUser"));
